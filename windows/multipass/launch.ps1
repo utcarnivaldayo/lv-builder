46b8adc135f125d49802112a4370d2914db29c94
utcarnivaldayo <ut091210.0495@gmail.com>
2022-10-20 01:05:20 +0900

    Modify launch.ps1

--- windows/multipass/launch.ps1
+++ windows/multipass/launch.ps1
@@ -5,11 +5,21 @@ $instance_name = $args[0]
 $cpus = $args[1]
 $disk = $args[2]
 $mem = $args[3]
-$cloud_init = $args[4]
+$cloud_init_path = $args[4]
 $image_name = $args[5]
+$local_workdir_fullpath = $args[6]
+$virtual_workdir_fullpath = $args[7]
 
-multipass launch --cpus $cpus --disk $disk --mem $mem --cloud-init $cloud_init --name $instance_name $image_name
-multipass exec $instance_name -- mkdir "sync"
+# launch multipass instance
+multipass launch --cpus $cpus --disk $disk --mem $mem --cloud-init "$cloud_init_path" --name $instance_name $image_name
+multipass exec $instance_name -- mkdir "$virtual_workdir_fullpath"
 multipass stop $instance_name
+multipass set local.privileged-mounts=true
+
+# ensure multipassd is running
+Restart-Service -Name multipassd
+
+# mount
+multipass mount "$local_workdir_fullpath" "$instance_name":"$virtual_workdir_fullpath"
 
 Write-Output "--- Multipass: Finish! ---"
