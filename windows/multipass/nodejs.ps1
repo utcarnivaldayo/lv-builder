89e3f4ec93c96067956bf8f62aa64c814421c6d5
utcarnivaldayo <ut091210.0495@gmail.com>
2022-11-04 00:56:43 +0900

    FIx var name

--- windows/multipass/nodejs.ps1
+++ windows/multipass/nodejs.ps1
@@ -1,4 +1,4 @@
 $instance_name = "nodejs"
 $local_workspace_dir = (Convert-Path ../../workspace/$instance_name)
 $virtual_workspace_dir = "/home/ubuntu/synced-$instance_name"
-pwsh -ExecutionPolicy Bypass ./launch.ps1 "${instance_name}" "2" "24G" "4G" "../../config/multipass/${instane_name}/${instance_name}.yaml" "22.04" "$local_workspace_dir" "$virtual_workspace_dir"
+pwsh -ExecutionPolicy Bypass ./launch.ps1 "${instance_name}" "2" "24G" "4G" "../../config/multipass/${instance_name}/${instance_name}.yaml" "22.04" "$local_workspace_dir" "$virtual_workspace_dir"
